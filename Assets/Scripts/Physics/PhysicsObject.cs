762576b5f6c0330dafff83600312b9496a8b5577
quartz55 <john.razor97@gmail.com>
2017-04-28 18:13:43 +0100

    Lets start over

---
+++ Assets/Scripts/Physics/PhysicsObject.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Physics
+{
+    public class PhysicsObject : MonoBehaviour
+    {
+        public float GravityModifier = 1f;
+
+        // Movement
+        protected Rigidbody2D Rb;
+        protected Vector2 Velocity;
+        protected Vector2 TargetVelocity;
+
+        // Collisions
+        protected Vector2 GroundNormal;
+
+        private void Start()
+        {
+            Rb = GetComponent<Rigidbody2D>();
+        }
+
+
+        private void Update()
+        {
+            TargetVelocity = Vector2.zero;
+            ComputeVelocity();
+        }
+
+        protected virtual void ComputeVelocity()
+        {
+        }
+
+        private void FixedUpdate()
+        {
+            Velocity += GravityModifier * Physics2D.gravity * Time.deltaTime;
+            Velocity.x = TargetVelocity.x;
+
+            var deltaPosition = Velocity * Time.deltaTime;
+
+            Rb.MovePosition(Rb.position + deltaPosition);
+        }
+    }
+}
---
+++ Assets/ragdoll.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ragdoll : MonoBehaviour {
+
+
+    private Rigidbody2D _rb;
+
+    private ParticleSystem _boost;
+    private bool _boosting;
+    private float _moving;
+
+    public float Speed = 500;
+    public float JetpackForce = 650;
+
+    private AudioSource source;
+
+
+    // Use this for initialization
+    void Start () {
+        _rb = GetComponent<Rigidbody2D>();
+        _boost = transform.Find("Boost").gameObject.GetComponent<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	        _boost.Play();
+	        source.PlayOneShot(source.clip, 1);
+
+	}
+
+    private void FixedUpdate()
+    {
+        _rb.AddForce(new Vector2(1,1) * Speed);
+    }
+}
